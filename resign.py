c49bd30cb8d76a98742effefa9d6bafe2cc5a1e7
Haizhou Wang <haizhou@haizhoumac.local>
2016-06-22 20:59:35 +0800

    sign

--- resign.py
+++ resign.py
@@ -37,7 +37,7 @@ def unzip(src,des):
 
 # 高亮输出
 def print_red(msg):
-    print '\033[0;31m', msg, '\033[0m'
+    print 'error: ', '\033[0;31m', msg, '\033[0m'
     return
 
 def print_green(msg):
@@ -67,13 +67,18 @@ def findconfig(dirname):
     print_red('安装包'+ dirname + '中' +'检索config.json文件失败')
     return 0
 
+# 删除文件夹
+def removeDir(dirname):
+    if isWindows():
+        os.system("rd /s /q " + dirname)
+    else:
+        os.system("rm -rf " + dirname)
+
+
 # 清理作案现场
 def restoreENV():
     print_green('清理作案现场...')
-    if isWindows():
-        os.system("rd /s /q " + UNPACK_DIR)
-    else:
-        os.system("rm -rf " + UNPACK_DIR)
+    removeDir(UNPACK_DIR)
 
 # 向json文件中写入指定字段
 def writeJson(filename, parname, value):
@@ -90,15 +95,37 @@ def writeJson(filename, parname, value):
         fp.write(filestr)
         fp.close()
         return 
-    
-    print_red('文件' + filename + '不存在!')
+    else:
+        print_red('文件' + filename + '不存在!')
 
+# 删除签名
+def removeSign():
+    for root, dirs, files in os.walk(UNPACK_DIR):
+        for dir in dirs:
+            if dir=='META-INF':
+                fulldir = os.path.join(root, dir)
+                print_green('删除签名...')
+                print_green(fulldir)
+                removeDir(fulldir)
+                print_green('签名删除成功！')
+                return
+    print_red('签名删除失败！')
+
+# 签名
+def signpack(packname):
+    execstr = "jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore " + KEYSTORE + " -storepass " + PASSWORD + " " + NEW_PACK + " " + ALIAS
+    status = os.system(execstr)
 
 # main
 if len(sys.argv) <> 3:
     print_red('参数错误')
     print_green('eg. python resign.py 2333 d:/redbird.apk')
     sys.exit(1)
+
+# 签名账号
+KEYSTORE = 'customer-hn787878-20160315.keystore'
+ALIAS = 'customer'
+PASSWORD = 'hn787878'
 
 # 母包
 ORIGIN_PACK = sys.argv[2]
@@ -109,14 +136,15 @@ SIGN_CODE = sys.argv[1]
 # 签名包
 NEW_PACK = newname(SIGN_CODE, ORIGIN_PACK)
 
-# 解压查找config.json文件
+# 解压目录
+UNPACK_DIR = NEW_PACK[:NEW_PACK.rindex('.')]
 print_green('解包中...')
 print_green(ORIGIN_PACK)
-UNPACK_DIR = NEW_PACK[:NEW_PACK.rindex('.')]
 unzip(ORIGIN_PACK, UNPACK_DIR)
 print_green('成功解压到: ')
 print_green(UNPACK_DIR)
 
+# 查找配置文件
 print_green('定位config.json文件...')
 CONFIG_FILE = findconfig(UNPACK_DIR)
 print_green('找到配置文件: ')
@@ -126,7 +154,17 @@ print_green(CONFIG_FILE)
 print_green('写入代理商字段...')
 writeJson(CONFIG_FILE, 'agent', SIGN_CODE)
 
+# 删除旧签名
+removeSign()
+
+# 压缩
+print_green('重新打包中...')
+zip_dir(UNPACK_DIR, NEW_PACK)
 print_green(NEW_PACK)
 
+# 签名
+print_green('重新签名中...')
+
+# 清理作案现场
 restoreENV()
 
