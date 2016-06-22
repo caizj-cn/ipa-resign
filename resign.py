72f0bd9239f70fdadba9debbe8a34ae708e165f3
Haizhou Wang <haizhou@haizhoumac.local>
2016-06-22 18:59:19 +0800

    agent

--- resign.py
+++ resign.py
@@ -3,6 +3,7 @@
 import zipfile
 import sys, os
 import platform
+import json
 
 # 是否windows
 def isWindows():
@@ -56,22 +57,76 @@ def newname(prefex, oldname):
     index=oldname.rindex('.')
     return oldname[:index] + '_' + prefex + oldname[index:]
 
+# 查找config文件
+def findconfig(dirname):
+    for root, dirs, files in os.walk(dirname):
+        for name in files:
+            if 'config.json' == name:
+                return os.path.join(root, name)
+    
+    print_red('安装包'+ dirname + '中' +'检索config.json文件失败')
+    return 0
+
+# 清理作案现场
+def restoreENV():
+    print_green('清理作案现场...')
+    if isWindows():
+        os.system("rd /s /q " + UNPACK_DIR)
+    else:
+        os.system("rm -rf " + UNPACK_DIR)
+
+# 向json文件中写入指定字段
+def writeJson(filename, parname, value):
+    if os.path.isfile(filename):
+        fp = open(filename, 'r')
+        filestr = fp.read()
+        fp.close
+
+        dictstr = json.loads(filestr)
+        dictstr[parname] = value
+        filestr = json.dumps(dictstr, ensure_ascii=False)
+
+        fp = open(filename, 'w')
+        fp.write(filestr)
+        fp.close()
+        return 
+    
+    print_red('文件' + filename + '不存在!')
+
+
 # main
 if len(sys.argv) <> 3:
     print_red('参数错误')
     print_green('eg. python resign.py 2333 d:/redbird.apk')
     sys.exit(1)
 
+# 母包
 ORIGIN_PACK = sys.argv[2]
+
+# 代理商id
 SIGN_CODE = sys.argv[1]
+
+# 签名包
 NEW_PACK = newname(SIGN_CODE, ORIGIN_PACK)
-UNPACK_DIR = os.path.dirname(os.path.abspath(ORIGIN_PACK))
 
-print_red(NEW_PACK)
-print_red(UNPACK_DIR)
-
+# 解压查找config.json文件
+print_green('解包中...')
+print_green(ORIGIN_PACK)
+UNPACK_DIR = NEW_PACK[:NEW_PACK.rindex('.')]
 unzip(ORIGIN_PACK, UNPACK_DIR)
+print_green('成功解压到: ')
+print_green(UNPACK_DIR)
 
+print_green('定位config.json文件...')
+CONFIG_FILE = findconfig(UNPACK_DIR)
+print_green('找到配置文件: ')
+print_green(CONFIG_FILE)
 
+# 写入代理商字段
+print_green('写入代理商字段...')
+writeJson(CONFIG_FILE, 'agent', SIGN_CODE)
 
+print_green(NEW_PACK)
+
+restoreENV()
 
